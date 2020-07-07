757392996e4c924be27d427c6ad2f03a94e8c695
mikaelaliwanag <mikaela301@gmail.com>
2020-07-06 21:27:52 -0700

    Connect to new database

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
 ///// home route //////
 app.get("/", function(req, res) {
   res.render("home", {
